f5ef943c58af2b99fdd2814cbd216e4f37d7bb43
Ryan Abusaada <71282474+ryanabusa8134@users.noreply.github.com>
2023-06-24 11:29:45 -0400

    Food Data
    - Added a script that allows us to create food objects on the fly
    - If you want to create a new instance just right click and create an object as you normally would and find "Food" as the first item. Then you can adjust its field data

---
+++ Assets/Scripts/EnemyData/Food.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName ="Food")]
+public class Food : ScriptableObject
+{
+    public string name;
+    public string value;
+    public Sprite foodSprite;
+}
