45a9e4a13265dbb173c30661eb85f4147969e943
Grace-Shao <graceshao203@gmail.com>
2023-06-23 23:13:26 -0400

    Wave spawner works, spawns few enemies each wave.
    will fix that null ptr exception at the end of the wave later.

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -18,20 +18,41 @@ public class WaveSpawner : MonoBehaviour
 
     private Wave currentWave; // current wave and int (1st, 2nd, or 3rd wave..)
     private int currentWaveNumber;
+    private float nextSpawnTime; // time it takes to spawn next enem
+
+    private bool canSpawn = true;
 
     private void Update()
     {
         // takes a wave from waves and makes it currentWave
         currentWave = waves[currentWaveNumber];
         SpawnWave();
+        // GameObject[] totalEnemies = GameObject.FindGameObjectsWithTag("Customer");
+        // diff
+        if (!canSpawn)
+        {
+            currentWaveNumber++;
+            canSpawn = true;
+        }
     }
 
     // takes random enemy and spawns at random position
     void SpawnWave()
     {
-        GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
-        // choose a random spawnpt
-        Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
-        Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
+        if (canSpawn && nextSpawnTime < Time.time) //Time.time = time passed in seconds, since game start [to check time interval of spawn]
+        {
+            GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
+            // choose a random spawnpt
+            Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
+            // only spawn the num we want to spawn
+            currentWave.noOfEnemies--;
+            // 12:03 timer for spawning next enem
+            nextSpawnTime = Time.time + currentWave.spawnInterval;
+            if (currentWave.noOfEnemies == 0)
+            {
+                canSpawn = false;
+            }
+        }
     }
 }
