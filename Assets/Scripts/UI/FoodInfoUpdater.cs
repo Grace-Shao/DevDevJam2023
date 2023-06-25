c47d6f3cbf809754df8f1942e3c27e54e6b1e143
Ryan Abusaada <71282474+ryanabusa8134@users.noreply.github.com>
2023-06-25 15:14:05 -0400

    yummy cooldown

--- Assets/Scripts/UI/FoodInfoUpdater.cs
+++ Assets/Scripts/UI/FoodInfoUpdater.cs
@@ -8,13 +8,29 @@ public class FoodInfoUpdater : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI[] m_textFields;
     [SerializeField] private Image[] m_imageFields;
+    private Color cooldown_color;
+    private void Start()
+    {
+        cooldown_color = m_imageFields[0].color;
+    }
 
     private void Update()
     {
         for (int i = 0; i < m_textFields.Length; i++)
         {
             m_textFields[i].text = FoodStorage.Instance.FoodList[i].ammo.ToString();
-            m_imageFields[i].fillAmount = FoodStorage.Instance.FoodList[i].currCooldown / FoodStorage.Instance.FoodList[i].cooldown;
+
+            if (FoodStorage.Instance.FoodList[i].ammo <= 0 && FoodStorage.Instance.FoodList[i].currCooldown <= 0)
+            {
+                m_imageFields[i].color = Color.gray;
+                m_imageFields[i].fillAmount = 1;
+            }       
+            else
+            {
+                m_imageFields[i].color = cooldown_color;
+                m_imageFields[i].fillAmount = FoodStorage.Instance.FoodList[i].currCooldown / FoodStorage.Instance.FoodList[i].cooldown;
+            }
+                
         }
     }
 }
