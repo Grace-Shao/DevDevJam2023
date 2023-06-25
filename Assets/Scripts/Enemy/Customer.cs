48f5a2c5617646f4e1d9d24dac7ff3ce5e3b781c
Ryan Abusaada <71282474+ryanabusa8134@users.noreply.github.com>
2023-06-24 20:41:16 -0400

    Boss Things and Ponits

--- Assets/Scripts/Enemy/Customer.cs
+++ Assets/Scripts/Enemy/Customer.cs
@@ -8,9 +8,6 @@ public class Customer : MonoBehaviour
     [SerializeField] private CustomerData m_customerData;
     [SerializeField] private CustomerHud m_customerHud;
 
-    public static Score _score;
-    
-
     public CustomerData CustomerData
     {
         get { return m_customerData; }
@@ -22,33 +19,28 @@ public class Customer : MonoBehaviour
         set { m_foodData = value; }
     }
 
-    private void Start() {
-        _score = FindObjectOfType<Score>();
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var foodProj = collision.GetComponent<FoodProjectile>();
         if (foodProj != null) 
         {
+            Destroy(gameObject.GetComponent<Collider2D>());
             StartCoroutine(ReactToFood(foodProj));
         }
     }
-    // TODO: Adjust Player Points using the Point System script based on the value of the food
     private IEnumerator ReactToFood(FoodProjectile foodProj)
     {
         if (foodProj.foodData.name == m_foodData.name) 
         {
             // Increment points to pointsystem based on m_foodData.value
             m_customerHud.Satisfied();
-            Destroy(gameObject.GetComponent<Collider2D>());
-            _score.scoreNum += 10;
+            Score.Instance.SCORE += foodProj.foodData.value;
         }
         else
         {
             // Decrement points to pointsystem based on m_foodData.value
             m_customerHud.Unsatisfied();
-            Destroy(gameObject.GetComponent<Collider2D>());
-            _score.scoreNum -= 10;
+            Score.Instance.SCORE -= foodProj.foodData.value;
         }
         yield return new WaitForSeconds(1);
         Destroy(gameObject);
--- Assets/Scripts/Score/Score.cs
+++ Assets/Scripts/Score/Score.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 using TMPro;
 
 public class Score : MonoBehaviour
-{  
-    private TextMeshProUGUI _scoreText;
-    public int scoreNum;
-
-    // Start is called before the first frame update
-    void Start()
+{
+    #region Singleton
+    private static Score m_instance;
+    public static Score Instance
     {
-        _scoreText = FindObjectOfType<TextMeshProUGUI>();
+        get 
+        { 
+            if (m_instance == null)
+            {
+                m_instance = FindObjectOfType<Score>();
+            }
+            return m_instance; 
+        }
+    }
+    #endregion
+
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private int _scoreNum;
+
+    public int SCORE 
+    {
+        get { return _scoreNum; }
+        set { _scoreNum = value; }
     }
 
     // Update is called once per frame
     void Update()
     {
-        _scoreText.SetText($"Money: ${scoreNum}");
+        _scoreText.SetText($"Money: ${_scoreNum}");
     }
 }
