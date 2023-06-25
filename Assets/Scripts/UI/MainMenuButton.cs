0e65935459a530205618731861f7f3c1d87f3370
Carlijavier5 <carlijavier5@gmail.com>
2023-06-25 17:52:38 -0400

    credits + main menu fix

---
+++ Assets/Scripts/UI/MainMenuButton.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public enum ButtonType {
+        Play,
+        Credits,
+        Exit,
+        Back
+    } [SerializeField] ButtonType buttonType;
+    private Button button;
+
+    void Start() {
+        button = GetComponent<Button>();
+        switch (buttonType) {
+            case ButtonType.Play:
+                button.onClick.AddListener(Play);
+                break;
+            case ButtonType.Credits:
+                button.onClick.AddListener(Credits);
+                break;
+            case ButtonType.Exit:
+                button.onClick.AddListener(Exit);
+                break;
+            case ButtonType.Back:
+                button.onClick.AddListener(Back);
+                break;
+        }
+    }
+
+    private void Play() {
+        TransitionManager.Instance.LoadScene("Intro");
+    }
+
+    public void Credits() {
+        TransitionManager.Instance.LoadScene("Credits");
+    }
+
+    public void Exit() {
+        TransitionManager.Instance.Quit();
+    }
+
+    public void Back() {
+        TransitionManager.Instance.LoadScene("Title");
+        print("hello");
+    }
+}
